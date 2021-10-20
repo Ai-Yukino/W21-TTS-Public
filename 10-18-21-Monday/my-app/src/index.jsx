f11ee2add4bdf7ecaa2a8b3e761fa27aa3ea8e2a
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-10-20 07:41:34 -0500

    🐛 Renamed to jsx

--- 10-18-21-Monday/my-app/src/index.jsx
+++ 10-18-21-Monday/my-app/src/index.jsx
@@ -4,6 +4,9 @@ import Board from "./components/Board";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<Board />, document.getElementById("root"));
+console.log("hello");
+// https://stackblitz.com/edit/vite-ghbhiy?file=index.html
+// document.querySelector("#root").innerHTML = `<p>Hello world</p>`;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
